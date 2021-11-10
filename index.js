06515d14b402bfc6e61dd8a6b89718f3c1022a3f
Domeshow <domeshowemmanuel@gmail.com>
2021-11-10 15:44:55 +0200

    ft -  create crud routes

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 require('dotenv').config();
+const routes = require('./routes/index.js');
 
 const app = express();
+
+app.use('/api', routes);
+app.get('/', (req, res)=>{
+    res.status(200).send({ status: 200, message: `Welcome to the API. Go to http://localhost:${PORT}/api` })
+});
+
+app.use('**', (req, res)=>{
+    res.status(200).send({ status: 405, message: 'Resource requested not found on the server' })
+});
 
 const PORT = process.env.PORT || 3800;
 
 app.listen(PORT, ()=>{
-    console.log(`Server listening on port ${PORT}. Go to http://localhost:${PORT}`);
+    console.log(`Server listening on port ${PORT}. Go to http://localhost:${PORT}/api`);
 })
