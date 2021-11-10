7d186afc20519b76378b1b84dc6ba91dafb2add4
Domeshow <domeshowemmanuel@gmail.com>
2021-11-10 15:05:46 +0200

    ft - create server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3200;
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}. Go to http://localhost:${PORT}`);
+})
